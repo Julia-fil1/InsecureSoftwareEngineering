aa4172d06700ef3356e6430fc04b216db1ffa9a4
Julia-fil11 <47003930+Julia-fil1@users.noreply.github.com>
2022-02-17 01:53:22 +0000

    Adding database configurations.

--- src/main/java/com/a1/insecureswe/model/User.java
+++ src/main/java/com/a1/insecureswe/model/User.java
@@ -1,4 +1,23 @@
 package com.a1.insecureswe.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
