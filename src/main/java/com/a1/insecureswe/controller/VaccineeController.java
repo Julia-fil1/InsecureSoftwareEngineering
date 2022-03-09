646992025385d865a5b2fe3a5f9827ff0936d655
L-Kerrigan <l.kerrigan@outlook.ie>
2022-03-09 20:07:37 +0000

    Appointments now book for current user, instead of hardcoding

--- src/main/java/com/a1/insecureswe/controller/VaccineeController.java
+++ src/main/java/com/a1/insecureswe/controller/VaccineeController.java
@@ -1,6 +1,7 @@
 package com.a1.insecureswe.controller;
 
 import com.a1.insecureswe.model.Forum;
+import com.a1.insecureswe.model.User;
 import com.a1.insecureswe.repository.ForumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.a1.insecureswe.exception.AppointmentTakenException;
@@ -10,12 +11,14 @@ import com.a1.insecureswe.repository.AppointmentRepository;
 import com.a1.insecureswe.repository.UserInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalTime;
 import java.util.List;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("vaccinee")
@@ -54,31 +58,51 @@ public class VaccineeController {
     public String history() { return "/vaccinee/history.html"; }
 
     @PostMapping("/set_appointment")
-    public String saveAppointment(Appointment appointment) throws AppointmentTakenException {
+    public String saveAppointment(Appointment appointment, Model model) throws AppointmentTakenException {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserInfo currentUser;
 
         String username;
-        if(principal instanceof UserInfo) {
-            username = ((UserInfo) principal).getUsername();
+        if(principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
         } else {
             username = principal.toString();
         }
         currentUser = userInfoRepository.findByUsername(username);
+        appointment.setUserId(currentUser.getId());
 
-        // Hardcoded till I fix this
-        appointment.setUserId(185L);
-        //appointment.setUserId(currentUser.getId());
+        // Ensure date isn't any earlier than today/now
+        if(appointment.getAppointmentDate().isBefore(LocalDate.now()) && appointment.getAppointmentTime().isBefore(LocalTime.now())) {
+            return "/vaccinee/book_appointment";
+        }
 
+        // Check to ensure user can't have more than 2 appointments (fully vaccinated)
+        /*if(currentUser.getAppointments().size() == 2) {
+            return "/";
+        }*/
+
+        // Check to make sure selected date & time aren't already taken at the selected location
         /*List<Appointment> appointments = appointmentRepository.findAll();
         for (Appointment app : appointments) {
-            if (app.getAppointmentDate() == appointment.getAppointmentDate() || appointment.getAppointmentDate().isBefore(LocalDate.now())) {
+            if (app.getAppointmentDate().isEqual(appointment.getAppointmentDate()) && app.getAppointmentTime().equals(appointment.getAppointmentTime()) && app.getLocation().equals(appointment.getLocation())) {
                 throw new AppointmentTakenException(app.getAppointmentDate());
             }
         }*/
         appointmentRepository.save(appointment);
         return "vaccinee/booking_success";
     }
+
+    // Thinking of displaying appointment details once booked, not done yet
+    /*@GetMapping("/vaccinee/booking_success")
+    public String booking_success(@PathVariable("appointment_id") Long id, Model model){
+        List<Appointment> appointments = appointmentRepository.findAll();
+        for (Appointment app : appointments) {
+            if (Objects.equals(app.getId(), id)) {
+                model.addAttribute(app);
+            }
+        }
+        return "vaccinee/booking_success";
+    }*/
 
     @GetMapping("/book_appointment")
     public String createAppointment(Model model) {
