03344db4d4b038c6223d95f3170b53ccdd5f4a23
Rolandg92 <roland.garza@ucdconnect.ie>
2022-03-11 09:10:42 +0000

    created stats section.

--- src/main/java/com/a1/insecureswe/repository/UserInfoRepository.java
+++ src/main/java/com/a1/insecureswe/repository/UserInfoRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     //    @Query("SELECT v FROM vaccinees v WHERE v.username = :username and v.password = :password")
-//    public UserInfo findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+//    public UserInfo findByUsernameAndPassword(@Param("username") String username, @Param("password") String password)
     UserInfo findByUsername(String username);
+
+    @Query("select count(v.nationality) from vaccinees v where v.nationality = 'Irish'")
+    Long findTotalIrish();
+
 }
