2a4482b6561530df4641ce80ed0c84a65a321485
Julia-fil11 <47003930+Julia-fil1@users.noreply.github.com>
2022-03-11 09:45:01 +0000

    Started on implementing enabling staff to edit the dose of a vaccinee.

--- src/main/java/com/a1/insecureswe/repository/UserInfoRepository.java
+++ src/main/java/com/a1/insecureswe/repository/UserInfoRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     //    @Query("SELECT v FROM vaccinees v WHERE v.username = :username and v.password = :password")
 //    public UserInfo findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
     UserInfo findByUsername(String username);
+
+    @Override
+    Optional<UserInfo> findById(Long aLong);
 }
