3b4264c76fc5d5a759d0b7604b283d0534f7f584
Julia-fil11 <47003930+Julia-fil1@users.noreply.github.com>
2022-05-17 12:10:57 +0100

    Adding necessary security configs

--- src/main/java/com/a1/insecureswe/SecurityConfiguration.java
+++ src/main/java/com/a1/insecureswe/SecurityConfiguration.java
@@ -1,19 +1,24 @@
 package com.a1.insecureswe;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
+import com.a1.insecureswe.security.JWTFilter;
+import com.a1.insecureswe.security.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -22,6 +27,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
+
+    @Autowired
+    private MyUserDetailsService uds;
+
+    @Autowired
+    private JWTFilter filter;
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -37,6 +48,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+                .csrf().disable()
+                .httpBasic().disable()
+                .cors()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .antMatchers("/vaccinee/**").hasAuthority("VACCINEE")
@@ -49,7 +64,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .and()
-                .exceptionHandling().accessDeniedPage("/adminOnly");
+                .exceptionHandling().accessDeniedPage("/adminOnly")
+                .and()
+                .userDetailsService(uds)
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, authException) ->
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+                )
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
     }
 
@@ -67,6 +94,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public AuthenticationFailureHandler AuthenticationFailureHandler() {
         return new CustomAuthenticationFailureHandler();
 
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
